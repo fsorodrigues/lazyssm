72e4503fdfb78bbbbc0c9351ba8da552cf8afa3b
fsorodrigues <fsorodrigues@gmail.com>
2026-04-28 16:47:24 +1200

    feat: centralize app logging and relocate process output

--- process/process.go
+++ process/process.go
@@ -4,72 +4,69 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 type Proc struct {
-	Name     string
-	Cmd      string
-	PID      int
-	Outfile  string
-	Status   string
-	Process  *exec.Cmd
-	LogFile  *os.File
-	LastLine string
+	Name          string
+	Cmd           string
+	PID           int
+	Outfile       string
+	Status        string
+	Process       *exec.Cmd
+	LogFile       *os.File
+	LastLine      string
+	ProcessLogDir string
 }
 
 func (p *Proc) Run() {
-	// 1. Create or open the log file
 	logFileName := fmt.Sprintf("process-%s.log", p.Name)
-	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o666)
+	logFilePath := filepath.Join(p.ProcessLogDir, logFileName)
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o666)
 	if err != nil {
 		panic(err)
 	}
 
-	// 2. Define command
 	cmd := exec.Command("./simulate_service.sh", p.Name, "5")
-
-	// 3. Set output to the log file
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
 
-	// 4. Start the command (does not wait for completion)
 	if err := cmd.Start(); err != nil {
 		logFile.Close()
 		panic(err)
 	}
 
-	// 5. Store references for later cleanup
 	p.Process = cmd
 	p.PID = cmd.Process.Pid
 	p.Status = "running"
 	p.LogFile = logFile
-	p.Outfile = logFileName
-	log.Printf("Background process started with PID: %d. Logging to %s\n", p.PID, logFileName)
+	p.Outfile = logFilePath
+	slog.Info("background process started", "name", p.Name, "pid", p.PID, "process_output_file", logFilePath)
 }
 
 func (p Proc) Kill() {
 	if p.PID == 0 {
-		log.Printf("No PID set for process %q; nothing to kill\n", p.Name)
+		slog.Warn("no PID set for process; nothing to kill", "name", p.Name)
 		return
 	}
 
 	proc, err := os.FindProcess(p.PID)
 	if err != nil {
-		log.Printf("Could not find process %d: %v\n", p.PID, err)
+		slog.Error("could not find process", "name", p.Name, "pid", p.PID, "error", err)
 		return
 	}
 
 	if err := proc.Signal(os.Interrupt); err != nil {
-		log.Printf("Failed to send SIGINT to %d, attempting SIGKILL: %v\n", p.PID, err)
+		slog.Warn("failed to send SIGINT; attempting SIGKILL", "name", p.Name, "pid", p.PID, "error", err)
 		if err := proc.Kill(); err != nil {
-			log.Printf("Failed to kill process %d: %v\n", p.PID, err)
+			slog.Error("failed to kill process", "name", p.Name, "pid", p.PID, "error", err)
 		}
 	}
 
-	log.Printf("Process %d (%s) killed\n", p.PID, p.Name)
+	slog.Info("process killed", "name", p.Name, "pid", p.PID)
 
 	if p.LogFile != nil {
 		p.LogFile.Close()
@@ -82,24 +79,20 @@ func (p *Proc) RefreshOutput() string {
 	if p.Outfile == "" {
 		return ""
 	}
+
 	f, err := os.Open(p.Outfile)
 	if err != nil {
 		return ""
 	}
 	defer f.Close()
 
-	// Seek to the end and scan backwards for the last non-empty line.
 	var lastLine string
 	stat, err := f.Stat()
 	if err != nil || stat.Size() == 0 {
 		return ""
 	}
 
-	// Read from end, up to 4KB should be enough for last line
-	readSize := int64(4096)
-	if stat.Size() < readSize {
-		readSize = stat.Size()
-	}
+	readSize := min(stat.Size(), int64(4096))
 	_, _ = f.Seek(-readSize, io.SeekEnd)
 
 	scanner := bufio.NewScanner(f)
